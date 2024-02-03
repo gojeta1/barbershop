de409921337920dbbb1e3629aa9c83f6d087901f
guilhermebarbosa <guilhermebarbosa19@gmail.com>
2024-02-03 15:00:17 -0300

    feat: add ServiceItem component

--- app/barbershops/[id]/_components/barbershop-info.tsx
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image'
 import { useRouter } from "next/navigation";
 
 interface BarbershopInfoProps {
-    barbershop: Barbershop;
+    barbershop: Barbershop
 }
 const BarbershopInfo = ({barbershop}: BarbershopInfoProps) => {
 
--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { db } from "@/app/_lib/ prisma";
 import BarbershopInfo from "./_components/barbershop-info";
 import ServiceItem from "./_components/service-item";
+import { Key } from "react";
 
 interface BarbershopDetailsPageProps {
     params: {
@@ -28,12 +29,14 @@ const BarbershopDetailsPage = async ({params}: BarbershopDetailsPageProps) => {
     }
     return ( 
         <div>
-        <BarbershopInfo barbershop={barbershop}/>
+            <BarbershopInfo barbershop={barbershop}/>
 
-        {barbershop.services.map((service: any) => (
-          <ServiceItem key={service.id} service={service} />
-        ))}
-        
+            <div className="px-5 flex flex-col gap-4 py-6">
+                {barbershop.services.map((service: any) => (
+                <ServiceItem key={service.id} service={service} />
+                ))}
+            </div>
+
         </div>
 
      );
