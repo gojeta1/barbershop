ff5af7fc5024c5b85fa8a64aa913097e4eadd705
Guilherme Barbosa <guilhermebarbosa19@gmail.com>
2024-02-05 10:43:04 -0300

    feat: ensure confirmedbookings are show from home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,10 +6,28 @@ import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
 import { db } from "../_lib/ prisma";
 import BarbershopItem from "./_components/barbershop-item";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default async function Home() {
 
-  const barbershop = await db.barbershop.findMany({})
+  const session = await getServerSession(authOptions)
+
+  const [barbershops, confirmedBookings] = await Promise.all([
+    db.barbershop.findMany({}),
+    session?.user? db.booking.findMany({
+      where: {
+        userId: (session.user as any).id,
+        date: {
+          gte: new Date(),
+        }
+      },
+      include: {
+        service: true,
+        barbershop: true,
+      }
+    }) : Promise.resolve([]),
+  ]);
 
   return (
   <div>
@@ -25,30 +43,39 @@ export default async function Home() {
     <div className="px-5 mt-6">
       <Search />
     </div>
-    <div className="px-5 mt-6">
-      <h2 className="text-xs mb-3  uppercase text-gray-400 font-bold">Agendamentos</h2>
-      {/* <BookingItem/> */}
+
+    <div className="mt-6">
+
+      <h2 className="pl-5 text-xs mb-3  uppercase text-gray-400 font-bold">Agendamentos</h2>
+
+      <div className="px-5 flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+       {confirmedBookings.map(booking => <BookingItem key={booking.id} booking={booking}/>)}
+      </div>
+
     </div>
     <div className="mt-6">
+
       <h2 className="px-5 text-xs mb-3  uppercase text-gray-400 font-bold">Recomendados</h2>
 
       <div className="px-5 flex gap-2 overflow-x-auto [&::-webkit-scrollbar]:hidden ">
-        {barbershop.map((barbershop:any) => (
+        {barbershops.map((barbershop:any) => (
         <BarbershopItem key={barbershop.id} barbershop={barbershop}/>
         ))}
       </div>
 
     </div>
     <div className="mt-6 mb-[4.5rem]">
+
       <h2 className="px-5 text-xs mb-3  uppercase text-gray-400 font-bold">Populares</h2>
 
       <div className="px-5 flex gap-2 overflow-x-auto [&::-webkit-scrollbar]:hidden ">
-        {barbershop.map((barbershop:any) => (
+        {barbershops.map((barbershop:any) => (
         <BarbershopItem key={barbershop.id} barbershop={barbershop}/>
         ))}
       </div>
 
     </div>
+
   </div>
   );
 }
--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -17,7 +17,9 @@ const Header = () => {
     return ( 
         <Card>
             <CardContent className="p-5 justify-between items-center flex flex-row">
-                <Image src="/logo.png" alt="FSW Barber" height={22} width={120}/>
+                <Link href="/">
+                    <Image src="/logo.png" alt="FSW Barber" height={22} width={120}/>
+                </Link>
                 <Sheet>
                     <SheetTrigger asChild>
                     <Button variant="outline" size="icon">
