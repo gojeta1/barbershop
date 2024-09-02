519a3462fa9363d50804ea6cb7bd4c903bb54757
gojeta1 <guilhermebarbosa19@gmail.com>
2024-09-02 15:54:07 -0300

    adasdsadas

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -40,7 +40,7 @@ export default async function Home() {
 
       <div className="px-5 pt-5">
         <h2 className="text-xl font-bold">
-          {session?.user ? `Olá, ${session.user.name?.split(" ")[0]}!` : "Olá! Vamos agendar um corte hoje?"}
+          {/* {session?.user ? `Olá, ${session.user.name?.split(" ")[0]}!` : "Olá! Vamos agendar um corte hoje?"} */}
         </h2>
         <p className="capitalize text-sm">
           {format(new Date(), "EEEE',' dd 'de' MMMM", {
